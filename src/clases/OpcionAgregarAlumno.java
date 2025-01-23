f54a5f6fc7d01a6fb87bd5d2e101d68769025845
alicia-2004 <aliciaclasegradosuperior@gmail.com>
2025-01-23 13:32:12 +0100

    Cuarto cambio

---
+++ src/clases/OpcionAgregarAlumno.java
@@ -0,0 +1,10 @@
+package clases;
+
+public class OpcionAgregarAlumno implements IOpcionMenu {
+    @Override
+    public void ejecutar(GestorAlumnos gestorAlumnos) {
+        // Solicitar los datos del alumno y agregarlo
+        Alumno nuevoAlumno = new Alumno("Juan", 20);
+        gestorAlumnos.añadirAlumno(nuevoAlumno);
+    }
+}
