a1d7379d408622ec4a7e5fe6f0ea5e0cc85aec3d
kev12s <kev12sanz@gmail.com>
2025-01-23 13:51:50 +0100

    sexto cambio

---
+++ src/clases/OpcionMostrarAlumno.java
@@ -0,0 +1,11 @@
+package clases;
+
+public class OpcionMostrarAlumnos implements IOpcionMenu {
+    @Override
+    public void ejecutar(GestorAlumnos gestorAlumnos) {
+        // Mostrar la lista de alumnos utilizando GestorAlumnos
+        gestorAlumnos.listarAlumnos();
+        int i=0;
+    }
+}
+
